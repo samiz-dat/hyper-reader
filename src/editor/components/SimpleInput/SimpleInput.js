e6d78f8bc9cc80aa8be8030cb020b62bed895434
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-15 21:03:18 +1000

    experimentation with styling

--- src/editor/components/SimpleInput/SimpleInput.js
+++ src/editor/components/SimpleInput/SimpleInput.js
@@ -3,16 +3,16 @@ import { Component } from 'substance'
 export default class SimpleInput extends Component {
   render ($$) {
     const { label, name, type, pattern } = this.props
-    let el = $$('div').addClass('sc-input')
+    let el = $$('div').addClass('sc-input a-flex a-stretch-content')
     let input = $$('input')
-      .addClass('a-inline')
+      .addClass('a-flex a-grow-1 a-mr1 a-b0 a-bb a-serif')
       .attr({
         name,
         pattern,
         type: type || 'text'
       })
     let labelEl = $$('label')
-      .addClass('a-inline')
+      .addClass('a-flex a-grow-1 a-mr1')
       .attr({
         for: name
       })
