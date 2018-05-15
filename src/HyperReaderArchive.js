e1ebd9636eeabfa812724646035a3d1baf8075fd
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-15 19:06:45 +1000

    pass hr instance correctly to hr2Dom function

--- src/HyperReaderArchive.js
+++ src/HyperReaderArchive.js
@@ -103,7 +103,7 @@ export default class HyperReaderArchive {
   async _load (key) {
     const hr = this.manager.get(key)
     // await hr.import('# hello\n\nworld', { type: 'md' })
-    const dom = await hr2Dom(hr)
+    const dom = await hr2Dom(hr.hr)
     return ArticleLoader.load(dom, this.configurator, { archive: this })
   }
 }
