1cb41d308553ae813450c3959db5302512b612b8
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-29 14:19:23 +1000

    hide open button unless completely downloaded; todo: make this pretty

--- src/ui/components/ListItem.js
+++ src/ui/components/ListItem.js
@@ -4,6 +4,7 @@ import Button from '../../editor/components/Button/Button'
 export default class ListItem extends Component {
   render ($$) {
     const { key, title, folder, speed, size, hr, authorised } = this.props
+    const downloading = size.totalPercentage !== 100
     let el = $$('li')
       .addClass('pa3')
       .ref('li')
@@ -17,7 +18,7 @@ export default class ListItem extends Component {
         $$('span').append('can edit:' + authorised)
       ),
       $$('div').addClass('mono f7').append(key).ref('key'),
-      $$(Button, { text: 'Open', icon: 'edit', size: 'small' }).on('click', () => { this.send('hr:open', { key }) }),
+      !downloading && $$(Button, { text: 'Open', icon: 'edit', size: 'small' }).on('click', () => { this.send('hr:open', { key }) }),
       $$(Button, { text: 'Show', icon: 'folder', size: 'small', status: 'secondary' }).on('click', () => { this.send('hr:reveal', { folder }) }),
       $$(Button, { icon: 'trash', size: 'small', status: 'secondary' }).on('click', () => { this.send('hr:remove', { key }) })
     )
