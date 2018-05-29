5c3c352308c5b0392c61880c3fa7bffb391b8d0f
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-29 15:49:51 +1000

    open up comment box on creation

--- src/editor/components/Comment/CommentCommand.js
+++ src/editor/components/Comment/CommentCommand.js
@@ -8,6 +8,15 @@ import { AnnotationCommand } from 'substance'
 class CommentCommand extends AnnotationCommand {
   canFuse () { return false }
   canDelete () { return false }
+
+  executeCreate (params) {
+    let result = super.executeCreate(params)
+    let editorSession = this._getEditorSession(params)
+    editorSession.transaction((tx) => {
+      tx.setSelection(tx.selection.collapse())
+    })
+    return result
+  }
 }
 
 export default CommentCommand
