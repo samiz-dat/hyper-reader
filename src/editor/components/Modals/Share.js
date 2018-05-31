610722a477b070bbffb6db393afd8d0a34b66737
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-05-31 20:14:25 +1000

    update text about sharing reading lists

--- src/editor/components/Modals/Share.js
+++ src/editor/components/Modals/Share.js
@@ -1,8 +1,14 @@
 import { Component } from 'substance'
+import Button from '../Button/Button'
 
 export default class Share extends Component {
   render ($$) {
-    var el = $$('div').append('simply share this key' + this.props.key)
+    var el = $$('div').append(
+      'Share this unique key with a friend to allow them to download this reading list.',
+      $$('div').addClass('mono f5 break-all fw900 bg-light-gray br2 pa2 mv3').append(this.props.key).ref('key'),
+      // $$(Button, { text: 'Copy to Clipboard' }).addClass('a-block center mb3').on('click', this._copy.bind(this)),
+      'Anyone with this key will be able to access the reading lists.'
+    )
     return el
   }
 }
