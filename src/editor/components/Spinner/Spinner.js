7cb8330c57acb693e41d10ac7f860365d816df4a
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-06-03 18:26:52 +1000

    fix #30

--- src/editor/components/Spinner/Spinner.js
+++ src/editor/components/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import { Component } from 'substance'
 export default class Spinner extends Component {
   render ($$) {
     var el = $$('div')
-      .addClass('table abs vh-100 w-100 bg-black-70 top-0 z-max')
+      .addClass('table fixed vh-100 w-100 bg-black-70 top-0 z-max')
       .append(
         $$('div').addClass('table-cell v-mid tc').append($$('i').addClass('white faa fa-spinner fa-pulse f1'))
       )
